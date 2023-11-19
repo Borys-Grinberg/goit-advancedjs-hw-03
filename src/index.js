a4559d1e57ce420f74ae132b0033eea7bf2cea53
Borys-Grinberg <borys.grinberg@gmail.com>
2023-11-19 23:58:46 +0100

    ups

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+// index.js
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import iziToast from 'izitoast';
+
+// index.js
+// ... existing code ...
 
 document.addEventListener('DOMContentLoaded', async function () {
   const breedSelect = document.querySelector('.breed-select');
   const catInfoDiv = document.querySelector('.cat-info');
+  const loader = document.querySelector('.loader');
 
   try {
     const breeds = await fetchBreeds();
 
     breedSelect.addEventListener('change', async function () {
       const selectedBreedId = breedSelect.value;
+
+      // Show loader while fetching cat information
+      loader.style.display = 'inline-block';
+
       const catData = await fetchCatByBreed(selectedBreedId);
 
+      // Hide loader after fetching cat information
+      loader.style.display = 'none';
+
+      // Display cat information
       catInfoDiv.innerHTML = `
-        <p><strong>Breed:</strong> ${catData[0].breeds[0].name}</p>
-        <p><strong>Description:</strong> ${catData[0].breeds[0].description}</p>
-        <p><strong>Temperament:</strong> ${catData[0].breeds[0].temperament}</p>
-        <img src="${catData[0].url}" alt="Cat Image">
+        <img src="${catData[0].url}" alt="Cat Image" class="cat-image">
+        <div class="cat-details">
+          <h2>${catData[0].breeds[0].name}</h2>
+          <p class="description">${catData[0].breeds[0].description}</p>
+          <p><strong>Temperament:</strong> ${catData[0].breeds[0].temperament}</p>
+        </div>
       `;
     });
   } catch (error) {
@@ -24,6 +39,8 @@ document.addEventListener('DOMContentLoaded', async function () {
     showErrorToast('Oops! Something went wrong! Try reloading the page!');
   }
 });
+
+// ... existing code ...
 
 function showErrorToast(message) {
   iziToast.error({
