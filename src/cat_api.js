7989133b2078d4b5f0c7d683887642c70ec7b883
Borys-Grinberg <borys.grinberg@gmail.com>
2023-11-25 22:47:00 +0100

    uffa

--- src/cat_api.js
+++ src/cat_api.js
@@ -1,77 +1,27 @@
-import axios from 'axios';
-import SlimSelect from 'slim-select';
+// cat_api.js
 
-axios.defaults.headers.common['x-api-key'] =
+import axios from 'axios';
+
+const apiKey =
   'live_7zO2UtHss6WanH1oLQ1vnbzQS31rHHFk6zvi69lyordiqDagXpIxykJooBldDqh8';
 
-// Helper functions
-function showLoader() {
-  showElement('.loader');
-}
-
-function hideLoader() {
-  hideElement('.loader');
-}
-
-function showElement(selector) {
-  document.querySelector(selector).classList.remove('hidden');
-}
-
-function hideElement(selector) {
-  document.querySelector(selector).classList.add('hidden');
-}
-
-function handleFetchError() {
-  showElement('.error');
-  setTimeout(() => {
-    hideElement('.error');
-  }, 5000); // Hide error after 5 seconds, adjust as needed
-}
+axios.defaults.headers.common['x-api-key'] = apiKey;
 
 export function fetchBreeds() {
-  showLoader();
-  hideElement('.cat-info');
-  hideElement('.error');
-
-  return axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => {
-      hideLoader();
-      showElement('.breed-select');
-      const breedSelect = new SlimSelect({
-        select: '.breed-select',
-        placeholder: 'Select a breed',
-        data: response.data.map(breed => ({
-          text: breed.name,
-          value: breed.id,
-        })),
-      });
-      return response.data;
-    })
-    .catch(error => {
-      console.error('Error fetching cat breeds:', error);
-      handleFetchError();
-      throw error;
-    });
+  return new Promise((resolve, reject) => {
+    axios
+      .get('https://api.thecatapi.com/v1/breeds')
+      .then(response => resolve(response.data))
+      .catch(error => reject(error));
+  });
 }
 
 export function fetchCatByBreed(breedId) {
-  showLoader();
-  hideElement('.breed-select');
-  hideElement('.cat-info');
-  hideElement('.error');
-
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-  return axios
-    .get(url)
-    .then(response => {
-      hideLoader();
-      showElement('.cat-info');
-      return response.data;
-    })
-    .catch(error => {
-      console.error('Error fetching cat by breed:', error);
-      handleFetchError();
-      throw error;
-    });
+  return new Promise((resolve, reject) => {
+    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+    axios
+      .get(url)
+      .then(response => resolve(response.data))
+      .catch(error => reject(error));
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,75 @@
-import { fetchBreeds, fetchCatByBreed } from './cat_api.js';
-import iziToast from 'izitoast';
+import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed } from './cat_api';
+const breedSelect = document.querySelector('.breed-select');
+const catInfoDiv = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+let selectedBreedId;
 
-document.addEventListener('DOMContentLoaded', async function () {
-  const breedSelect = document.querySelector('.breed-select');
-  const catInfoDiv = document.querySelector('.cat-info');
-  const loader = document.querySelector('.loader');
-
-  try {
-    const breeds = await fetchBreeds();
-
-    breedSelect.addEventListener('change', async function () {
-      const selectedBreedId = breedSelect.value;
-
-      // show loader
-      loader.style.display = 'inline-block';
-
-      const catData = await fetchCatByBreed(selectedBreedId);
-
-      // Hide loader after fetching cat information
-      loader.style.display = 'none';
-
-      // Display cat information
-      catInfoDiv.innerHTML = `
-        <img src="${catData[0].url}" alt="Cat Image" class="cat-image">
-        <div class="cat-details">
-          <h2>${catData[0].breeds[0].name}</h2>
-          <p class="description">${catData[0].breeds[0].description}</p>
-          <p><strong>Temperament:</strong> ${catData[0].breeds[0].temperament}</p>
-        </div>
-      `;
-    });
-  } catch (error) {
-    console.error('Error:', error);
-    showErrorToast('Oops! Something went wrong! Try reloading the page!');
-  }
+const slim = new SlimSelect({
+  select: breedSelect,
 });
+
+slim.setData([]);
+
+breedSelect.addEventListener('change', function () {
+  selectedBreedId = breedSelect.value; // Змінена змінна, щоб зберегти значення
+
+  // Show loader while fetching cat information
+  showLoader();
+
+  const fetchPromise = selectedBreedId
+    ? fetchCatByBreed(selectedBreedId)
+    : Promise.resolve();
+
+  fetchPromise
+    .then(catData => {
+      // Check if catData is defined and has the expected structure
+      if (
+        catData &&
+        Array.isArray(catData) &&
+        catData.length > 0 &&
+        catData[0].breeds &&
+        catData[0].breeds.length > 0
+      ) {
+        const catBreed = catData[0].breeds[0];
+
+        // Display cat information
+        catInfoDiv.innerHTML = `
+          <div class="cat-info-inner">
+            <img src="${catData[0].url}" alt="Cat Image" class="cat-image">
+            <div class="cat-details">
+              <h2>${catBreed.name}</h2>
+              <p class="description">${catBreed.description}</p>
+              <p><strong>Temperament:</strong> ${catBreed.temperament}</p>
+            </div>
+          </div>
+        `;
+      } else {
+        // Handle the case when catData is not as expected
+        showErrorToast(
+          'Oops! Unable to retrieve cat information. Please try again.'
+        );
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      showErrorToast('Oops! Something went wrong! Try reloading the page!');
+    })
+    .finally(() => {
+      // Hide loader after fetching cat information
+      hideLoader();
+    });
+});
+
+function showLoader() {
+  loader.style.display = 'inline-block';
+  catInfoDiv.style.display = 'none';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+  catInfoDiv.style.display = 'flex';
+}
 
 function showErrorToast(message) {
   iziToast.error({
@@ -44,3 +79,16 @@ function showErrorToast(message) {
     timeout: 5000,
   });
 }
+
+// Fetch breeds and update SlimSelect outside of the event listener
+fetchBreeds()
+  .then(breeds => {
+    // Update SlimSelect with breed data
+    slim.setData(breeds.map(breed => ({ text: breed.name, value: breed.id })));
+    // Hide loader after fetching breed information
+    hideLoader();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    showErrorToast('Oops! Something went wrong! Try reloading the page!');
+  });
